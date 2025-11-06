e203ef7c592a1d1f082d2b8f8b2b0fab05797e52
hirpinus <dott.antoniodellasala@gmail.com>
2025-11-06 23:54:54 +0000

    Commentato il codice per l'inserimento di SRC_DIR nel sys.path

--- tests/test_class.py
+++ tests/test_class.py
@@ -6,8 +6,8 @@ ROOT = Path(__file__).resolve().parents[1]          # root della repo
 SRC_DIR = ROOT / "src"
 MODEL_DIR = ROOT / "models"
 
-if str(SRC_DIR) not in sys.path:
-    sys.path.insert(0, str(SRC_DIR))
+#if str(SRC_DIR) not in sys.path:
+#    sys.path.insert(0, str(SRC_DIR))
 
 from sentiment_predict import SentimentPredictor
 
